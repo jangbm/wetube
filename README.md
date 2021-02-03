bdd4a3c6e7fa971fef43e03f4e40b7c694bc5dd1
jangbm <jang2620@naver.com>
2021-02-03 09:38:45 +0900

    #2.25 More Controlers

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # wetube
+
 cloning Youtube with Vanilla and NodeJs
+
+## Pages :
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- app.js
+++ app.js
@@ -11,13 +11,16 @@ import {localsMiddleware} from "./middlewares";
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan("dev")); 
-
+app.use(function(req, res, next) {
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
+    return next();
+    });
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
