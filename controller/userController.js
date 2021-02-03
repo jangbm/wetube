ceec04295aade3e10dd1fd56479f5477d603ffc5
jangbm <jang2620@naver.com>
2021-02-03 14:12:35 +0900

    #2.9 setup

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,7 @@
 import routes from "../routes";
-export const getJoin  = (req,res) => {
+export const getJoin = (req, res) => {
 
-    res.render("join",{ pageTitle : "Join"});
+    res.render("join", { pageTitle: "Join" });
 }
 
 export const postJoin = (req, res) => {
@@ -13,32 +13,34 @@ export const postJoin = (req, res) => {
             password2
         }
     } = req;
-    if(password !== password2){
+    if (password !== password2) {
         res.status(400);
-        res.render("join", {pageTitle: "Join"});
+        res.render("join", { pageTitle: "Join" });
     } else {
         res.redirect(routes.home);
     }
 }
 
 
-export const getLogin  = (req,res) => res.render("login",{ pageTitle : "Login"});
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
 
-export const postLogin = (req, res) =>{
+export const postLogin = (req, res) => {
     res.redirect(routes.home);
 }
 
-export const logout  = (req,res) => {
+export const logout = (req, res) => {
     //to do : logout
+    //hello
+    //in ubuntu
     res.redirect(routes.home);
 }
 
 
 
-export const users = (req,res) => res.render("users",{ pageTitle : "Users"});
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 
-export const userDetail = (req,res) => res.render("userDetail",{ pageTitle : "User Detail"});
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 
-export const editProfile = (req,res) => res.render("editProfile",{ pageTitle : "Edit Profile"});
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 
-export const changePassword = (req,res) => res.render("changePassword",{ pageTitle : "Change Password"});
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
