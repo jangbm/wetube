19c165595ba7ff49a559d668652aec39b2c3f32f
jangbm <jang2620@naver.com>
2021-02-18 10:32:56 +0900

    #3.10 deleting a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
-import { deleteVideo, editVideo, getUpload, postUpload, videoDetail } from "../controller/videoController";
+import { deleteVideo, getEditVideo,postEditVideo, getUpload, postUpload, videoDetail } from "../controller/videoController";
 import routes from "../routes";
 import {uploadVideo} from "../middlewares";
 
 const videoRouter = express.Router();
 
+//upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+//Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
